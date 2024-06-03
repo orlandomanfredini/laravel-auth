77709a670144b67b20260eacde586e64aafc9188
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-03 17:34:53 +0200

    ADD: migrate

--- database/migrations/2024_06_03_152545_create_projects_table.php
+++ database/migrations/2024_06_03_152545_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 180);
+            $table->string('slug');
+            $table->text('description');
             $table->timestamps();
         });
     }
