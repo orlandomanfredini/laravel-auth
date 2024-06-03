da88bd1ca9e84b6c28bff470609657b89e82433d
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-03 19:02:43 +0200

    ADD:faker

--- database/migrations/2024_06_03_152545_create_projects_table.php
+++ database/migrations/2024_06_03_152545_create_projects_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 180);
+            $table->string('title', 180)->unique();
             $table->string('slug');
             $table->text('description');
             $table->timestamps();
