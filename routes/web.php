d780f0494b082824aafcd64c33cf1e0addb27a8b
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-03 17:01:13 +0200

    ADD: rinominate route /admin and routeServiceProvider cont HOME = /admin

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/admin', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
